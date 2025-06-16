af450044c9ab67c44e41d24941cb34e2fadc994e
brilop <brifdlopes@gmail.com>
2025-06-16 18:58:40 -0300

    feat: create routes for ibge api and people

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IBGEController;
+use App\Http\Controllers\PersonController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/states', [IBGEController::class, 'states']);
+Route::get('/states/{uf}/cities', [IBGEController::class, 'cities']);
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\PersonController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('people')->group(function () {
+    Route::get('/', [PersonController::class, 'index'])->name('people.index');
+    Route::get('/create', [PersonController::class, 'create'])->name('people.create');
+    Route::post('/store', [PersonController::class, 'store'])->name('people.store');
+    Route::get('/{id}/edit', [PersonController::class, 'edit'])->name('people.edit');
+    Route::get('/{id}/show', [PersonController::class, 'show'])->name('people.show');
+    Route::put('/{id}/update', [PersonController::class, 'update'])->name('people.update');
+    Route::delete('/{id}/deletar', [PersonController::class, 'destroy'])->name('people.delete');
 });
